588172887ece686395464b772f32cc14fc87623f
ionlyfordream <smilehailiang@gmail.com>
2015-05-22 19:28:49 +0800

    将application改为app

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define('APP_DEBUG',True);
 // define('BIND_MODULE','Develop');
 // 定义应用目录
-define('APP_PATH','./Application/');
+define('APP_PATH','./App/');
 define('APP_LANG', true);
 // 定义版本信息
 define('VERSION', 'v2.2.1');
